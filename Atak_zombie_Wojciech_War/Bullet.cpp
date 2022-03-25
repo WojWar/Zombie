79983a032ffa19152acd9ef43629938342f3885b
Wojciech Wargula <w.wargula@protonmail.com>
2022-03-25 21:47:38 +0100

    bullets are in list now instead of vector

--- Atak_zombie_Wojciech_War/Bullet.cpp
+++ Atak_zombie_Wojciech_War/Bullet.cpp
@@ -14,11 +14,24 @@ Bullet::Bullet(Player &_player):
 
 		velocity.x = -0.3f;
 	}
+
+	is_shooted_value = false;
+	to_be_deleted = false;
 }
 
 
 Bullet::~Bullet()
 {
+	velocity.x = 0;
+}
+
+void Bullet::set_to_delete() {
+
+	to_be_deleted = true;
+}
+bool Bullet::is_to_delete() {
+
+	return to_be_deleted;
 }
 
 
@@ -28,6 +41,7 @@ bool Bullet::is_wall(char _tab[][576])
 	if (_tab[(int)(this->getPosition().x)][(int)(this->getPosition().y)]) 
 	{
 		return true; 
+		this->~Bullet();
 	}
 	else
 	{
@@ -37,9 +51,6 @@ bool Bullet::is_wall(char _tab[][576])
 
 bool Bullet::is_zombie(std::vector<Zombie*> &_vectorZombies)
 {
-
-	
-		is_shooted_value = false;
 		for (int i = 0; i < _vectorZombies.size(); i++) {
 			if (_vectorZombies[i]->getGlobalBounds().contains(this->getPosition()))
 			{
--- Atak_zombie_Wojciech_War/Bullet.h
+++ Atak_zombie_Wojciech_War/Bullet.h
@@ -11,8 +11,14 @@ public:
 	~Bullet();
 	sf::Vector2f velocity;
 
+	//methods
 	bool is_wall(char _tab[][576]);
 	bool is_zombie(std::vector<Zombie*> &_vectorZombies);
+	void set_to_delete();
+	bool is_to_delete();
+
+	//attributes
 	bool is_shooted_value;
+	bool to_be_deleted;
 };
 
